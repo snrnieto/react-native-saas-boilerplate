2938ceaadd203abbcd6b1f40532a6f4f6778f696
Sebastian Nieto <snrnieto1999@hotmail.com>
2026-01-15 16:52:16 -0500

    feat: Integrate NativeWind and Tailwind CSS for styling, including configuration files and initial app layout.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,7 +5,10 @@ import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
+    <View className='flex-1 items-center justify-center gap-4'>
+      <Text className="text-3xl font-bold !text-red-500 bg-red-300 p-10 rounded-full">
+        Welcome to Nativewind!
+      </Text>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
@@ -14,11 +17,11 @@ export default function TabOneScreen() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  // container: {
+  //   flex: 1,
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
